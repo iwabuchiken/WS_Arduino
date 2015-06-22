270eeebc3194a7da159067240577cbd749aa13e3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-06-23 08:47:39 +0900

    D-2
    v-1.0#3
    ---
    1. An LED and a push button: p.34
    
    	==> done

---
+++ D-2/example_02/example_02.ino
@@ -0,0 +1,35 @@
+// example_02.ino
+/*
+ * LED on while the button being pushed
+ * 
+ */
+
+const int LED		= 13;
+const int BUTTON	= 7;
+
+int val				= 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+	pinMode(LED, OUTPUT);
+	pinMode(BUTTON, INPUT);
+	
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+	val = digitalRead(BUTTON);
+	
+	if (val == HIGH) {
+		
+		digitalWrite(LED, HIGH);
+		
+	} else {//if (val == HIGH)
+		
+		digitalWrite(LED, LOW);
+		
+	}//if (val == HIGH)
+	
+}

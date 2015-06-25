0f55e5e0a3f37cee2eb7515d23ad99c248aa4b73
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-06-25 15:00:11 +0900

    D-2
    v-3.0#1
    ---
    1. PWM
    
    	==> done

---
+++ D-2/example_04__PWM/example_04__PWM.ino
@@ -0,0 +1,40 @@
+// example_04.ino
+//	p.49 Banzi 
+/*
+ * 1. LED
+ * 		=> PWM
+ */
+
+const int LED		= 9;	// pin number
+
+int i				= 0;	// loop
+
+const int delay_Time	= 10;	// delay time in millsec
+
+void setup() {
+  // put your setup code here, to run once:
+	
+	pinMode(LED, OUTPUT);	// LED => output
+
+}
+
+void loop() {
+	
+	//for(i = 0; i < 255; i = i * 2) {
+  for(i = 0; i < 255; i ++) {
+		
+		analogWrite(LED, i);
+		
+		delay(delay_Time);
+		
+	}
+	
+//	for(i = 255; i > 0; i --) {
+//		
+//		analogWrite(LED, i);
+//		
+//		delay(delay_Time);
+//		
+//	}
+	
+}//void loop()

4a07e94e1f5f1e3b0f1377dc4e7282160d769fbe
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-06-25 14:46:38 +0900

    D-2
    v-2.0#4
    ---
    1. bouncing => avoid
    
    	==> done

---
+++ D-2/example_03C/example_03C.ino
@@ -0,0 +1,58 @@
+// example_03C.ino
+//	p.41 Banzi 
+/*
+ * 1. push button
+ * 	=> LED keeps on
+ * 2. Bouncing	=> countermeasure done
+ */
+
+const int LED		= 13;	// pin number
+const int BUTTON	= 7;	// pin number
+
+int val				= 0;	// status of the input pin
+
+int old_val				= 0;	// previous value of 'val'
+
+int state			= 0;	// status of LED
+
+const int delay_Time = 100;  // in millseconds
+
+void setup() {
+  // put your setup code here, to run once:
+	
+	pinMode(LED, OUTPUT);	// LED => output
+	pinMode(BUTTON, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+	val = digitalRead(BUTTON);	// read the input; store it in the val
+	
+	// if HIGH => change the state
+	if ((val == HIGH) & (old_val == LOW)) {
+
+		state = 1 - state;
+
+		delay(delay_Time);
+//		delay(10);
+		
+	}//if (val == HIGH)
+	
+	// store 'val'
+	old_val = val;
+	
+	// if the state of the LED is on, i.e. 1 => HIGH
+	//	otherwise => LOW
+	if (state == 1) {
+
+		digitalWrite(LED, HIGH);
+
+	} else {//if (state == 1)
+		
+		digitalWrite(LED, LOW);
+		
+	}//if (state == 1)
+	
+}//void loop()

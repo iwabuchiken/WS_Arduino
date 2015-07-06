6b0a2cb0c60ad5690d26f630dcb3c265fc666cda
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-07-06 14:30:27 +0900

    D-5
    s-2#2
    ---
    1. analog-in value => Cds
    
    	==> done

---
+++ D.1-49/D-5_lcd/D_5_s_2_2_LCD_analog_value/D_5_s_2_2_LCD_analog_value.ino
@@ -0,0 +1,38 @@
+/*
+  * D_5_s_2_LCD_2_lines.ino
+  *
+  * REF: http://happy-arduino.blogspot.jp/2012/02/lcd_26.html
+  * 
+  * 1. LCD, analog values
+  */
+
+  #include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
+
+//int LCD_BL=6;
+
+void setup()
+{
+//  pinMode(LCD_BL,OUTPUT);
+  
+  lcd.begin(16, 2);//16x2桁
+  lcd.clear();
+  
+}
+
+void loop()
+{
+// digitalWrite(LCD_BL,HIGH);
+  int anain ;
+  lcd.setCursor(0, 0);
+  lcd.print("CDS Input Value");
+
+  anain = analogRead(0) ;  // CDSを接続したアナログ０番ピンを読み取る
+
+  delay(500);
+  lcd.setCursor(0, 1);
+  lcd.print(anain);
+  
+}
+

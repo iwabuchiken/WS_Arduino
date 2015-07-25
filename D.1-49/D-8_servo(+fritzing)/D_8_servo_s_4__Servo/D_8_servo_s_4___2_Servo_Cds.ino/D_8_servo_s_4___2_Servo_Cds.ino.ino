5907942e649368457881ca21a7686322364f1258
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-07-25 15:54:18 +0900

    D-8
    s-4#2-2.t1
    ---
    1. servo	==> started moving

--- D.1-49/D-8_servo(+fritzing)/D_8_servo_s_4__Servo/D_8_servo_s_4___2_Servo_Cds.ino/D_8_servo_s_4___2_Servo_Cds.ino.ino
+++ D.1-49/D-8_servo(+fritzing)/D_8_servo_s_4__Servo/D_8_servo_s_4___2_Servo_Cds.ino/D_8_servo_s_4___2_Servo_Cds.ino.ino
@@ -13,13 +13,12 @@
 ///////////////////////////////////
 Servo servo1;
 
-char version[] = "8/4 #2 n.5";
-
-const char splash_Messsage[] = "Servo!!";
+char version[] = "8/4 #2-2 t1";
 
 const int SERVO_OUT_PIN    = 6;
 
 const int DELAY_TIME_MS    = 0;
+
 int deg=0;
 
 const int ANALOG_0 = 0;
@@ -86,106 +85,6 @@ void setup() {
 }//void setup()
 
 void loop() {
-  
-//  ///////////////////////////////////
-//  //
-//  // read: analog
-//  //
-//  ///////////////////////////////////
-//  val_0 = analogRead(ANALOG_0);
-//  val_1 = analogRead(ANALOG_1);
-//
-////  Serial.println(s0 + val_0);
-////  Serial.println(s1 + val_1);
-//  
-//  // difference
-//  diff = abs(val_0 - val_1);
-//  
-//  //debug
-//  String s("diff => ");
-//  Serial.println(s + diff);
-//
-////  // larger
-////  larger = larger_value(val_0, val_1);
-//  
-////  //debug
-////  String s2("larger => ");
-////  Serial.println(s2 + larger);
-//  
-//  ///////////////////////////////////
-////
-//// move: servo
-////
-/////////////////////////////////////
-//  if (diff > THRESH) {
-//    // larger
-//    larger = larger_value(val_0, val_1);
-//    
-//    //debug
-//    String s2("larger => ");
-//    Serial.println(s2 + larger);
-//    
-//    
-//    switch(larger) {
-//    
-//    case RIGHT:
-//      
-//      if (state != RIGHT) {
-//  
-//        state = RIGHT;
-//        
-//        move_Servo(state);
-//  
-//      }//if (state != RIGHT)
-//      
-//      break;
-//    
-//    case LEFT:
-//  
-//      if (state != LEFT) {
-//  
-//        state = LEFT;
-//        
-//        move_Servo(state);
-//  
-//      }//if (state != RIGHT)
-//      
-//      break;
-//  
-//      break;
-//      
-//    }
-//  
-//  }//if (diff > THRESH)
-//
-//  
-//  // wait
-//  delay(500);
-
-  
-//  // judge: diff is larger than THRESH?
-//  if (diff > THRESH) {
-//  
-//    update_Servo(val_0, val_1);
-//  
-//  } else {
-//    
-//    if (state == CENTER) {
-//      
-//      
-//      
-//    } else {//if (state == CENTER)
-//      
-//      // update state
-//      state = CENTER;
-//      
-//      // move servo
-//      move_Servo(state);
-//      
-//    }//if (state == CENTER)
-//  
-//    
-//  }
   
   ///////////////////////////////////
 //
@@ -269,4 +168,5 @@ int larger_value(int val_0, int val_1) {
   return (val_0 > val_1) ? RIGHT : LEFT;
   
 }
+
 

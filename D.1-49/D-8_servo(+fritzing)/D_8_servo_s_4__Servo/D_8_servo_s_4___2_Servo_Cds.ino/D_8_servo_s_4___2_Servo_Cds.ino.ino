193da7e48cf32b33c2c3e1f3de9eb0ee2a014d35
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-08-03 15:10:31 +0900

    D-8
    s-5#1
    ---
    1. re-try => s-4/#2-3
    
    	==> done

--- D.1-49/D-8_servo(+fritzing)/D_8_servo_s_4__Servo/D_8_servo_s_4___2_Servo_Cds.ino/D_8_servo_s_4___2_Servo_Cds.ino.ino
+++ D.1-49/D-8_servo(+fritzing)/D_8_servo_s_4__Servo/D_8_servo_s_4___2_Servo_Cds.ino/D_8_servo_s_4___2_Servo_Cds.ino.ino
@@ -1,244 +1,245 @@
-/*
-  * D_8_servo_s_4___2_Servo_Cds.ino
-  *
-  * 1. servo + 2 Cds
-  */
-
-#include <Servo.h>
-
-///////////////////////////////////
-//
-// vars
-//
-///////////////////////////////////
-Servo servo1;
-
-char version[] = "8/4 #2-3";
-
-const char splash_Messsage[] = "Servo!!";
-
-const int SERVO_OUT_PIN    = 6;
-
-const int DELAY_TIME_MS    = 0;
-int deg=0;
-
-const int ANALOG_0 = 0;
-const int ANALOG_1 = 1;
-
-int val_0;
-int val_1;
-
-String s0("A0 => ");
-String s1("A1 => ");
-
-// states
-int      state;
-const int CENTER  = 0;
-const int RIGHT = -1;
-const int LEFT  = 1;
-
-const int DEG_CENTER  = 90;
-const int DEG_RIGHT = 0;
-const int DEG_LEFT  = 180;
-
-int diff;
-int larger;
-
-const int THRESH    = 100;
-
-void setup() {
+  /*
+    * D_8_servo_s_4___2_Servo_Cds.ino
+    *
+    * 1. servo + 2 Cds
+    */
+  
+  #include <Servo.h>
   
   ///////////////////////////////////
   //
-  // serial
+  // vars
   //
   ///////////////////////////////////
-  Serial.begin(9600);
-
-  Serial.println(version);
+  Servo servo1;
   
-  ///////////////////////////////////
-//
-// servo
-//
-///////////////////////////////////
-  servo1.attach(SERVO_OUT_PIN);
+  char version[] = "8/4 #2-3 (8/5 #1)";
   
-  // initial moves
-  servo1.write(90);
+  const char splash_Messsage[] = "Servo!!";
   
-  delay(500);
+  const int SERVO_OUT_PIN    = 6;
   
-  servo1.write(0);
+  const int DELAY_TIME_MS    = 0;
+  int deg=0;
   
-  delay(100);
+  const int ANALOG_0 = 0;
+  const int ANALOG_1 = 1;
   
-  ///////////////////////////////////
-//
-// state
-//
-///////////////////////////////////
-  state = CENTER;
+  int val_0;
+  int val_1;
   
-  // move servo
-  move_Servo(state);
+  String s0("A0 => ");
+  String s1("A1 => ");
   
-}//void setup()
-
-void loop() {
+  // states
+  int      state;
+  const int CENTER  = 0;
+  const int RIGHT = -1;
+  const int LEFT  = 1;
   
-  ///////////////////////////////////
+  const int DEG_CENTER  = 90;
+  const int DEG_RIGHT = 0;
+  const int DEG_LEFT  = 180;
+  
+  int diff;
+  int larger;
+  
+  const int THRESH    = 100;
+  
+  void setup() {
+    
+    ///////////////////////////////////
+    //
+    // serial
+    //
+    ///////////////////////////////////
+    Serial.begin(9600);
+  
+    Serial.println(version);
+    
+    ///////////////////////////////////
   //
-  // read: analog
+  // servo
   //
   ///////////////////////////////////
-  val_0 = analogRead(ANALOG_0);
-  val_1 = analogRead(ANALOG_1);
-
-//  Serial.println(s0 + val_0);
-//  Serial.println(s1 + val_1);
-  
-  // difference
-  diff = abs(val_0 - val_1);
-  
-  //debug
-  String s("diff => ");
-  Serial.println(s + diff);
-//
-////  // larger
-////  larger = larger_value(val_0, val_1);
-//  
-////  //debug
-////  String s2("larger => ");
-////  Serial.println(s2 + larger);
-//  
+    servo1.attach(SERVO_OUT_PIN);
+    
+    // initial moves
+    servo1.write(90);
+    
+    delay(500);
+    
+    servo1.write(0);
+    
+    delay(100);
+    
+    ///////////////////////////////////
+  //
+  // state
+  //
   ///////////////////////////////////
-//
-// move: servo
-//
-///////////////////////////////////
-  if (diff > THRESH) {
-    // larger
-    larger = larger_value(val_0, val_1);
+    state = CENTER;
+    
+    // move servo
+    move_Servo(state);
+    
+  }//void setup()
+  
+  void loop() {
+    
+    ///////////////////////////////////
+    //
+    // read: analog
+    //
+    ///////////////////////////////////
+    val_0 = analogRead(ANALOG_0);
+    val_1 = analogRead(ANALOG_1);
+  
+    Serial.println(s0 + val_0);
+    Serial.println(s1 + val_1);
+    
+    // difference
+    diff = abs(val_0 - val_1);
     
     //debug
-    String s2("larger => ");
-    Serial.println(s2 + larger);
+    String s("diff => ");
+    Serial.println(s + diff);
+  //
+  ////  // larger
+  ////  larger = larger_value(val_0, val_1);
+  //  
+  ////  //debug
+  ////  String s2("larger => ");
+  ////  Serial.println(s2 + larger);
+  //  
+    ///////////////////////////////////
+  //
+  // move: servo
+  //
+  ///////////////////////////////////
+    if (diff > THRESH) {
+      // larger
+      larger = larger_value(val_0, val_1);
+      
+      //debug
+      String s2("larger => ");
+      Serial.println(s2 + larger);
+      
+      
+      switch(larger) {
+      
+      case RIGHT:
+        
+        if (state != RIGHT) {
     
+          state = RIGHT;
+          
+          move_Servo(state);
     
-    switch(larger) {
+        }//if (state != RIGHT)
+        
+        break;
+      
+      case LEFT:
+    
+        if (state != LEFT) {
+    
+          state = LEFT;
+          
+          move_Servo(state);
+    
+        }//if (state != RIGHT)
+        
+        break;
+    
+        break;
+        
+      }
+    
+    }//if (diff > THRESH)
+  
+    
+    // wait
+    delay(500);
+  
+    
+  //  // judge: diff is larger than THRESH?
+  //  if (diff > THRESH) {
+  //  
+  //    update_Servo(val_0, val_1);
+  //  
+  //  } else {
+  //    
+  //    if (state == CENTER) {
+  //      
+  //      
+  //      
+  //    } else {//if (state == CENTER)
+  //      
+  //      // update state
+  //      state = CENTER;
+  //      
+  //      // move servo
+  //      move_Servo(state);
+  //      
+  //    }//if (state == CENTER)
+  //  
+  //    
+  //  }
+    
+  }//void loop()
+  
+  void move_Servo(int state) {
+    
+    //debug
+    String s("move servo => ");
+    Serial.println(s + state);
+    
+    switch(state) {
+    
+    case CENTER:
+    
+      servo1.write(DEG_CENTER); break;
     
     case RIGHT:
-      
-      if (state != RIGHT) {
-  
-        state = RIGHT;
-        
-        move_Servo(state);
-  
-      }//if (state != RIGHT)
-      
-      break;
+    
+      servo1.write(DEG_RIGHT);  break;
     
     case LEFT:
-  
-      if (state != LEFT) {
-  
-        state = LEFT;
-        
-        move_Servo(state);
-  
-      }//if (state != RIGHT)
-      
-      break;
-  
-      break;
-      
-    }
-  
-  }//if (diff > THRESH)
-
-  
-  // wait
-  delay(500);
-
-  
-//  // judge: diff is larger than THRESH?
-//  if (diff > THRESH) {
-//  
-//    update_Servo(val_0, val_1);
-//  
-//  } else {
-//    
-//    if (state == CENTER) {
-//      
-//      
-//      
-//    } else {//if (state == CENTER)
-//      
-//      // update state
-//      state = CENTER;
-//      
-//      // move servo
-//      move_Servo(state);
-//      
-//    }//if (state == CENTER)
-//  
-//    
-//  }
-  
-}//void loop()
-
-void move_Servo(int state) {
-  
-  //debug
-  String s("move servo => ");
-  Serial.println(s + state);
-  
-  switch(state) {
-  
-  case CENTER:
-  
-    servo1.write(DEG_CENTER); break;
-  
-  case RIGHT:
-  
-    servo1.write(DEG_RIGHT);  break;
-  
-  case LEFT:
-  
-    servo1.write(DEG_LEFT); break;
-
-  default:
     
-    //debug
-    String s2("unknown state => ");
-  Serial.println(s2 + state);
-
+      servo1.write(DEG_LEFT); break;
+  
+    default:
+      
+      //debug
+      String s2("unknown state => ");
+    Serial.println(s2 + state);
+  
+    }
+    
   }
   
-}
-
-void update_Servo(int val_0, int val_1) {
-
-  // get: larger
-  larger = larger_value(val_0, val_1);
+  void update_Servo(int val_0, int val_1) {
   
-}
-
-///////////////////////////////////
-//
-// @param
-//  val_0 => right cds value
-//  val_1 => left cds value
-//
-// @return
-//  RIGHT or LEFT
-///////////////////////////////////
-int larger_value(int val_0, int val_1) {
-
-  return (val_0 > val_1) ? RIGHT : LEFT;
+    // get: larger
+    larger = larger_value(val_0, val_1);
+    
+  }
   
-}
-
+  ///////////////////////////////////
+  //
+  // @param
+  //  val_0 => right cds value
+  //  val_1 => left cds value
+  //
+  // @return
+  //  RIGHT or LEFT
+  ///////////////////////////////////
+  int larger_value(int val_0, int val_1) {
+  
+    return (val_0 > val_1) ? RIGHT : LEFT;
+    
+  }
+  
+  
 

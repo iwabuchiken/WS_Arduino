b098da6ab42acddd2915fc4afa20e2f4b4d9070b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-08-16 16:30:49 +0900

    D-12
    s-5#2-1
    ---
    1. avoid chattering
    
    	==> done

---
+++ D.1-49/D-12_irremote/D_12_irremote_s_5/D_12_irremote_s_5.ino
@@ -0,0 +1,180 @@
+  /*
+  * D_12_irremote_s_5.ino
+  *
+  *  REF: http://www.embedded-property.net/?p=986
+  *
+  * [Notices]
+  *   1. Monitor speed  => 57600 bps
+  *   2. Serial rate    => 57600
+  *   
+  * 
+  */
+
+///////////////////////////////////
+//
+// defines
+//
+///////////////////////////////////
+#define READ_PIN 7
+#define LOW_STATE 0
+#define HIGH_STATE 1
+
+const char title[] = "\n12/5 #2-1";
+
+///////////////////////////////////
+//
+// vars
+//
+///////////////////////////////////
+unsigned long now = micros();
+unsigned long lastStateChangedMicros = micros();
+int state = HIGH_STATE;
+
+///////////////////////////////////
+//
+// pins
+//
+///////////////////////////////////
+const int  LED_OUT = 8;
+int     MODE  = 0;
+const int   CHATTERING_TIME = 400;
+
+//////////////////////////////////////////////////////////////////////
+//
+// methods
+//
+//////////////////////////////////////////////////////////////////////
+//the Interrupt routine  (2)
+void interruptsw() {
+  
+  ///////////////////////////////////
+  //
+  // chattering
+  //
+  ///////////////////////////////////
+  delay(CHATTERING_TIME); // wait for 100 ms
+//  delay(200); // wait for 100 ms
+//  delay(100); // wait for 100 ms
+  
+  if (digitalRead(2) == 1) {
+
+    Serial.println("clicked!");
+
+  } else {//if (digitalRead(2) == 1)
+    
+    return;
+    
+  }//if (digitalRead(2) == 1)
+  
+  if(MODE){
+   MODE = 0;
+  }else{
+   MODE = 1;
+  }
+  
+}
+
+void waitLow() {
+    while (digitalRead(READ_PIN)==LOW) {
+      ;
+    }
+  }
+   
+int waitHigh() {
+  unsigned long start = micros();
+  while (digitalRead(READ_PIN)==HIGH) {
+    if (micros() - start > 5000000) {
+      return 1;
+    }
+  }
+  return 0;
+}
+  
+void setup()
+{
+//  Serial.begin(9600);
+  Serial.begin(57600);
+
+  ///////////////////////////////////
+  //
+  // pins
+  //
+  ///////////////////////////////////
+//  pinMode(7, INPUT);
+//  pinMode(READ_PIN,INPUT);
+  
+  pinMode(LED_OUT, OUTPUT);       
+  attachInterrupt(0, interruptsw, RISING );  // (1) => pin 2
+  
+  ///////////////////////////////////
+  //
+  // splash
+  //
+  ///////////////////////////////////
+  Serial.println(title);
+  
+}
+ 
+
+void loop() {
+
+  if(MODE){
+    digitalWrite(LED_OUT, LOW);
+    delay(100);               
+    
+        digitalWrite(LED_OUT, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(1000);               // wait for a second
+        digitalWrite(LED_OUT, LOW);    // turn the LED off by making the voltage LOW
+        delay(1000);               // wait for a second
+    }else{
+      digitalWrite(LED_OUT, HIGH); 
+    }
+  
+//  if (state == LOW_STATE) {
+//    waitLow();
+//  } else {
+//    int ret = waitHigh();
+//    if (ret == 1) {
+//      Serial.print("\n");
+//      return;
+//    }
+//  }
+//   
+//  now = micros();
+//  
+//  Serial.print((now - lastStateChangedMicros) / 10, DEC);
+//  
+//  ///////////////////////////////////
+//  //
+//  // show high or low
+//  //
+//  ///////////////////////////////////
+////  if (state == HIGH_STATE) {
+////    
+////    Serial.print(" HIGH");
+////    
+////  } else {
+////    
+////    Serial.print(" LOW");
+////    
+////  }
+//
+//  
+//  Serial.print(",");
+//  
+//  lastStateChangedMicros = now;
+//  if (state == HIGH_STATE) {
+//    state = LOW_STATE;
+//  } else {
+//    state = HIGH_STATE;
+//  }
+  
+}//void loop()
+
+
+
+
+
+
+
+

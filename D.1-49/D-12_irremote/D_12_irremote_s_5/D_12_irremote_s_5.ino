10de2819dfeb222a0455e742272b7a021dfb08c5
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-08-17 18:00:53 +0900

    D-12
    s-6#3-3
    ---
    1. use => DATA_POINT
    
    	==> done

--- D.1-49/D-12_irremote/D_12_irremote_s_5/D_12_irremote_s_5.ino
+++ D.1-49/D-12_irremote/D_12_irremote_s_5/D_12_irremote_s_5.ino
@@ -12,7 +12,9 @@
 
 #define IRpin       2    // 赤外線受信モジュール接続ピン番号
 
-const char splash[] = "12/6 #3-2";
+#define DATA_POINT  3
+
+const char splash[] = "12/6 #3-3";
 
 // 初期化処理
 void setup()
@@ -26,46 +28,11 @@ void setup()
 // メインの処理
 void loop()
 {
-     unsigned long t ;
-     int i , cnt ;
-     char IRbit[64] ;
+    int ans ;
 
-     t = 0 ;
-     if (digitalRead(IRpin) == LOW) {
-          // リーダ部のチェックを行う
-          t = micros() ;                          // 現在の時刻(us)を得る
-          while (digitalRead(IRpin) == LOW) ; // HIGH(ON)になるまで待つ
-          t = micros() - t ;          // LOW(OFF)の部分をはかる
-     }
-     // リーダ部有りなら処理する(3.4ms以上のLOWにて判断する)
-     if (t >= 3400) {
-          i = 0 ;
-          while(digitalRead(IRpin) == HIGH) ; // ここまでがリーダ部(ON部分)読み飛ばす
-          // データ部の読み込み
-          while (1) {
-               while(digitalRead(IRpin) == LOW) ; // OFF部分は読み飛ばす
-               t = micros() ;
-               cnt = 0 ;
-               while(digitalRead(IRpin) == HIGH) {// LOW(OFF)になるまで待つ
-                    delayMicroseconds(10) ;
-                    cnt++ ;
-                    if (cnt >= 1200) break ;    // 12ms以上HIGHのままなら中断
-               }
-               t = micros() - t ;
-               if (t >= 10000) break ;      // ストップデータ
-               if (t >= 1000)  IRbit[i] = (char)0x31 ;  // ON部分が長い
-               else            IRbit[i] = (char)0x30 ;  // ON部分が短い
-               i++ ;
-          }
-          // データ有りなら表示を行う
-          if (i != 0) {
-               IRbit[i] = 0 ;
-               DspData(i,IRbit) ;
-               
-//               Serial.println("has data");
-               
-          }
-     }
+    ans = IRrecive() ;                      // 赤外線リモコンのデータを受信する
+    if (ans != 0) Serial.println(ans,HEX) ; // リモコンからデータを受信したら表示する
+    
 }
 
 //受信データをシリアルモニタ画面に送り表示をさせる処理
@@ -87,3 +54,51 @@ void DspData(int num,char *data)
    }
    Serial.println(')') ;     
 }
+
+//赤外線リモコンのデータを受信する処理関数
+int IRrecive()
+{
+  unsigned long t ;
+  int i , j ;
+  int cnt , ans ;
+  char IRbit[64] ;
+
+  ans = 0 ;
+  t = 0 ;
+  if (digitalRead(IRpin) == LOW) {
+       // リーダ部のチェックを行う
+       t = micros() ;                          // 現在の時刻(us)を得る
+       while (digitalRead(IRpin) == LOW) ; // HIGH(ON)になるまで待つ
+       t = micros() - t ;         // LOW(OFF)の部分をはかる
+  }
+  // リーダ部有りなら処理する(3.4ms以上のLOWにて判断する)
+  if (t >= 3400) {
+       i = 0 ;
+       while(digitalRead(IRpin) == HIGH) ; // ここまでがリーダ部(ON部分)読み飛ばす
+       // データ部の読み込み
+       while (1) {
+            while(digitalRead(IRpin) == LOW) ;// OFF部分は読み飛ばす
+            t = micros() ;
+            cnt = 0 ;
+            while(digitalRead(IRpin) == HIGH) {// LOW(OFF)になるまで待つ
+                 delayMicroseconds(10) ;
+                 cnt++ ;
+                 if (cnt >= 1200) break ;   // 12ms以上HIGHのままなら中断
+            }
+            t = micros() - t ;
+            if (t >= 10000) break ;     // ストップデータ
+            if (t >= 1000)  IRbit[i] = (char)0x31 ; // ON部分が長い
+            else            IRbit[i] = (char)0x30 ; // ON部分が短い
+            i++ ;
+       }
+       // データ有りなら指定位置のデータを取り出す
+       if (i != 0) {
+            i = (DATA_POINT-1) * 8 ;
+            for (j=0 ; j < 8 ; j++) {
+                 if (IRbit[i+j] == 0x31) bitSet(ans,j) ;
+            }
+       }
+  }
+  return( ans ) ;
+}
+
